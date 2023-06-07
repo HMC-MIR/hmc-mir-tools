af56573bcdc263527af201965b8cd1f538abc0b5
Kavi Dey <kaviasher@gmail.com>
2023-06-07 10:30:53 -0700

    Clean up versioning code

--- src/hmc_mir/get_version.py
+++
@@ -1,5 +0,0 @@
-def myversion_func(version: ScmVersion):
-    from setuptools_scm.version import guess_next_version
-    print('myversion_func')
-    print(version)
-    return version.format_next_version(guess_next_version, '{guessed}b{distance}')
